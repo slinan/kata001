79ef07d3b661737d3fc920bb2b3147a4ef56bdb3
Santiago Liñán Romero <s.linan10@uniandes.edu.co>
2016-09-21 18:55:29 -0500

    Paso 1 - Se crea el test para cuando la cadena es vacía (ROJO)

---
+++ Solver.py
@@ -0,0 +1,5 @@
+import math
+
+class Solver:
+    def calcular(cadena):
+        pass
---
+++ SolverTest.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+
+import Solver
+
+class SolverTest(TestCase):
+    def test_calcular(self):
+        self.assertEqual(Solver().calcular(""),0,"Cadena vacía")
