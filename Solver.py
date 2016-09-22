2489ff5de33db85836d727f333bcc67c7b13ed11
Santiago Liñán Romero <s.linan10@uniandes.edu.co>
2016-09-21 20:32:40 -0500

    I2 - Paso 4 - Se crea el test para devolver un arreglo con el número de elementos y el menor elemento (n elementos) (AZUL  :) )

--- Solver.py
+++ Solver.py
@@ -5,19 +5,9 @@ class Solver:
         if cadena == "":
             return [0,'nan']
         else:
-            if ',' not in cadena:
-                return [1,int(cadena)]
-            elif len(cadena.split(','))==2:
-                cadSplit = cadena.split(',')
-                num1 = int(cadSplit[0])
-                num2 = int(cadSplit[1])
-                if(num1 > num2):
-                    return [2, num2]
-                else:
-                    return [2, num1]
-        cadenaSplit = cadena.split(',')
-        mini = int(cadenaSplit[0]);
-        for x in range (0, len(cadenaSplit)):
-            if(int(cadenaSplit[x]) < mini):
-                mini = int(cadenaSplit[x])
-        return [len(cadena.split(',')),mini]
+            cadenaSplit = cadena.split(',')
+            minimo = int(cadenaSplit[0]);
+            for x in range (1, len(cadenaSplit)):
+                if(int(cadenaSplit[x]) < minimo):
+                    minimo = int(cadenaSplit[x])
+            return [len(cadena.split(',')),minimo]
