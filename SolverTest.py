cf5dec5b85f2df14e719f923bb688ff864d22249
Santiago Liñán Romero <s.linan10@uniandes.edu.co>
2016-09-21 19:56:12 -0500

    Paso 4 - Se crea el test para devolver un arreglo con el número de elementos (n elementos) (VERDE  :) )

--- SolverTest.py
+++ SolverTest.py
@@ -13,4 +13,6 @@ class SolverTest(TestCase):
         self.assertEqual(Solver().calcular("4,2"), [2], "2 elementos")
 
     def test_calcular_n_elementos(self):
-        self.fail()
+        self.assertEqual(Solver().calcular("4,2,4,5,4,5,4,5,4"), [9], "9 elementos")
+        self.assertEqual(Solver().calcular("4,2,4,55,44,55,4,5,964"), [9], "9 elementos")
+        self.assertEqual(Solver().calcular("4,2,4,55,44,55,4,5,964,1,1,1,1,1,1,1,1"), [17], "17 elementos")
