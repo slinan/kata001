ef2b9b674ff0953b131eca7f2e0ef22db33a324e
Santiago Liñán Romero <s.linan10@uniandes.edu.co>
2016-09-21 19:54:25 -0500

    Paso 4 - Se crea el test para devolver un arreglo con el número de elementos (n elementos) (ROJO  :( )

--- SolverTest.py
+++ SolverTest.py
@@ -6,8 +6,11 @@ class SolverTest(TestCase):
     def test_calcular(self):
         self.assertEqual(Solver().calcular(""),[0],"Cadena vacía")
 
-    def test_calcular_numero_elementos(self):
+    def test_calcular_1_elementos(self):
         self.assertEqual(Solver().calcular("4"), [1], "1 elemento")
 
-    def test_calcular_numero_elementos(self):
+    def test_calcular_2_elementos(self):
         self.assertEqual(Solver().calcular("4,2"), [2], "2 elementos")
+
+    def test_calcular_n_elementos(self):
+        self.fail()
