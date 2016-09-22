324953005754095ce3347507199d8aed2c8e61eb
Santiago Liñán Romero <s.linan10@uniandes.edu.co>
2016-09-21 20:02:47 -0500

    I2 - Paso 1 - Se crea el test para devolver un arreglo con el número de elementos y el menor elemento (0 elementos) (VERDE  :) )

--- Solver.py
+++ Solver.py
@@ -3,6 +3,6 @@ import math
 class Solver:
     def calcular(self,cadena):
         if cadena == "":
-            return [0]
+            return [0,0]
         else:
             return [len(cadena.split(','))]
