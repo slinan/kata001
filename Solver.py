7910d420b5cc295b4d831cb03de6002ff60d1e36
Santiago Liñán Romero <s.linan10@uniandes.edu.co>
2016-09-23 18:35:25 -0500

    I3 - Paso 4 - Se crea el test para devolver un arreglo con el número de elementos, el menor elemento, el mayor (n elementos) (VERDE)

--- Solver.py
+++ Solver.py
@@ -15,10 +15,10 @@ class Solver:
                     return [2,int(cadenaSplit[0]),int(cadenaSplit[1])]
             cadenaSplit = cadena.split(',')
             minimo = int(cadenaSplit[0]);
-            #maximo = int(cadenaSplit[0]);
+            maximo = int(cadenaSplit[0]);
             for x in range (1, len(cadenaSplit)):
                 if(int(cadenaSplit[x]) < minimo):
                     minimo = int(cadenaSplit[x])
-            #    if(int(cadenaSplit[x])> maximo):
-            #        maximo = int(cadenaSplit[x])
-            return [len(cadena.split(',')),minimo]
+                if(int(cadenaSplit[x])> maximo):
+                    maximo = int(cadenaSplit[x])
+            return [len(cadena.split(',')),minimo, maximo]
